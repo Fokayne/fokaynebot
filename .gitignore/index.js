b660a6ab30a616b6f51a275eadecefe448d1fa93
Fokayne <34973713+Fokayne@users.noreply.github.com>
2017-12-31 02:38:43 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,13 @@ var bot = new Discord.Client();
 bot.on("ready", function(){
     bot.user.setGame("Créateur Fokayne");
     consol.log("Le bot à bien lancer");
+
+    client.on('message', msg => {
+        if (msg.content === 'créateur') {
+          msg.reply('Fokayne le plus beau !');
+        }
+      });
+
  });
 
  bot.login("Mzk2ODE5OTExODM5NzExMjUy.DSnDEw.iFPibcacgJUmajeN-4UXl59hlxE");
